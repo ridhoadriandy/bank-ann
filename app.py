acf193033586305c3632fa1958cb6997f128bb41
ridhoadriandy <85445668+ridhoadriandy@users.noreply.github.com>
2023-01-27 21:53:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,4 @@ def predict():
     
     return flask.render_template('index.html', predicted_value="Customer Churn rate: {}%".format(str(x)))
     # return jsonify({'prediction': str(x)})
-   
-if __name__ == '__main__':
-    return flask.render_template('index.html')
+ 
